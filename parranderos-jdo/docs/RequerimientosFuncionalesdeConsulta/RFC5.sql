9bf5c6213ddfd15e12030e03c2123e74256515d9
David-Fuq <d.fuquen@uniandes.edu.co>
2022-11-14 21:45:57 -0500

    carpetas

--- parranderos-jdo/docs/RequerimientosFuncionalesdeConsulta/RFC5.sql
+++
@@ -1,3 +0,0 @@
---RFC5 - MOSTRAR LAS COMPRAS HECHAS POR SUPERANDES A LOS PROVEEDORES
-
-select proveedores.nombre as proveedor, pedidos.*  from pedidos, proveedores where proveedores.nit = pedidos.proveedor;
--- parranderos-jdo/docs/RequerimientosFuncionalesdeConsulta/RFC6.sql
+++
@@ -1,3 +0,0 @@
---RFC6
-
-select clientes.nombre, compras.* from compras, clientes where compras.fecha>'15/10/22' and compras.fecha<'20/10/22' and compras.cliente = clientes.numdocumento; 
